1e62d8f78e439cc2dd26c7c65ee98247c82200d3
Tomotaka Sakuma <ktmtmks@gmail.com>
2016-12-23 22:13:06 +0900

    rename anim var name

--- src/app/github/github-issue/github-issue.component.ts
+++ src/app/github/github-issue/github-issue.component.ts
@@ -3,32 +3,28 @@ import { trigger, state, style, transition, animate } from '@angular/core';
 
 import { GithubIssue } from "../github-issue"
 
+const fadeIn = trigger('fadeInState', [
+  state('inactive', style({opacity: 0.1})),
+  state('active', style({opacity: 1.0})),
+  transition('inactive => active', animate('300ms ease-in')),
+  transition('active => inactive', animate('300ms ease-out'))
+])
+
 @Component({
   selector: 'github-issue',
   templateUrl: './github-issue.component.html',
   styleUrls: ['./github-issue.component.scss'],
-  animations: [
-    trigger('state', [
-      state('inactive', style({
-        opacity: 0.1,
-      })),
-      state('active', style({
-        opacity: 1.0,
-      })),
-      transition('inactive => active', animate('300ms ease-in')),
-      transition('active => inactive', animate('300ms ease-out'))
-    ])
-  ],
+  animations: [fadeIn],
 })
 export class GithubIssueComponent implements OnInit, AfterViewInit {
 
   @Input() issue: GithubIssue;
-  state: string = "inactive";
+  fadeInState: string = "inactive";
 
   ngOnInit() {
   }
 
   ngAfterViewInit() {
-    setTimeout(() => this.state = "active", 0);
+    setTimeout(() => this.fadeInState = "active", 0);
   }
 }
