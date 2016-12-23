3280e4c214c70c9d7287e2d5cfd85be472bf84e8
Tomotaka Sakuma <ktmtmks@gmail.com>
2016-12-23 10:54:16 +0900

    fade-in anim

--- src/app/github/github-issue/github-issue.component.ts
+++ src/app/github/github-issue/github-issue.component.ts
@@ -1,19 +1,34 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Input } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/core';
 
 import { GithubIssue } from "../github-issue"
 
 @Component({
   selector: 'github-issue',
   templateUrl: './github-issue.component.html',
-  styleUrls: ['./github-issue.component.scss']
+  styleUrls: ['./github-issue.component.scss'],
+  animations: [
+    trigger('state', [
+      state('inactive', style({
+        opacity: 0.1,
+      })),
+      state('active', style({
+        opacity: 1.0,
+      })),
+      transition('inactive => active', animate('300ms ease-in')),
+      transition('active => inactive', animate('300ms ease-out'))
+    ])
+  ],
 })
-export class GithubIssueComponent implements OnInit {
+export class GithubIssueComponent implements OnInit, AfterViewInit {
 
   @Input() issue: GithubIssue;
-
-  constructor() { }
+  state: string = "inactive";
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    setTimeout(() => this.state = "active", 0);
+  }
 }
